704df8abf44ddb5781cb7f87e83d3ab90e7ac114
Andy Pham <anpham-work@protonmail.com>
2023-08-11 13:01:52 +0000

    finished the challenge

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,16 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int insertIndex = 0;
+        boolean foundIndex = false;
+        while(foundIndex == false && insertIndex < nums.length){
+            if(target < nums[insertIndex]){
+                foundIndex = true;
+            }
+            else{
+                insertIndex++;
+            }
+        }
+        return insertIndex;
     }
 }
